4ef12f21db98e580521136716eca4e4b21afd142
Andrey Sarason <sarason@rambler.ru>
2024-10-29 21:55:05 +0300

    new file: src/Category.py, src/Product.py, src/main.py

---
+++ src/Category.py
@@ -0,0 +1,9 @@
+class Category:
+    name: str
+    description: str
+    products: list
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
---
+++ src/Product.py
@@ -0,0 +1,13 @@
+class Product:
+    name: str
+    discription: str
+    pris: float
+    quantity: int
+
+    def __init__(self, name, discription, prise, quantity):
+        self.name = name
+        self.discription = discription
+        self.prise = prise
+        self.quantity = quantity
+
+
