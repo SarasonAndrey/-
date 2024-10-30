848c151757a3badd1ffded370624ede1cc59100e
Andrey Sarason <sarason@rambler.ru>
2024-10-30 05:48:20 +0300

    modified: src/Category.py and src/Product.py

--- src/Category.py
+++ src/Category.py
@@ -1,9 +1,24 @@
 class Category:
-    name: str
-    description: str
-    products: list
+    name: str  # Название
+    description: str  # Описанме
+    products: list  # Список товаров по категориям
+    number_of_categories = 0  # Количество категорий
+    number_of_products = 0  # Количество товаров
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
+        Category.number_of_categories += 1
+        Category.number_of_products += 1
+
+
+if __name__ == "__main__":
+    categoty = Category("оборудование", "музыкальный", ["магнитола", "плеер"])
+    categoty = Category("мебель", "для спальни", ["кровать", "диван"])
+
+    print(categoty.name)
+    print(categoty.description)
+    print(categoty.products)
+    print(Category.number_of_categories)
+    print(Category.number_of_products)
--- src/Product.py
+++ src/Product.py
@@ -1,8 +1,8 @@
 class Product:
-    name: str
-    discription: str
-    pris: float
-    quantity: int
+    name: str  # Название
+    discription: str  # Описание
+    prise: float  # Цена
+    quantity: int  # Колличество в наличии
 
     def __init__(self, name, discription, prise, quantity):
         self.name = name
@@ -11,3 +11,10 @@ class Product:
         self.quantity = quantity
 
 
+if __name__ == "__main__":
+    product = Product("Патифон", "старый", 123.3, 3)
+
+    print(product.name)
+    print(product.discription)
+    print(product.prise)
+    print(product.quantity)
