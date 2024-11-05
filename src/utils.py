c4b401c37545ebcdc615cb126beeb4fb21a1cbb4
Andrey Sarason <sarason@rambler.ru>
2024-11-05 20:59:25 +0300

    modified: src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import json
 import os
-from typing import List
 
 from src.Category import Category
 from src.Product import Product
@@ -8,7 +7,7 @@ from src.Product import Product
 
 def read_json(path: str) -> dict:
     full_path = os.path.abspath(path)
-    with open(full_path, 'r', encoding='utf-8') as file:
+    with open(full_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
@@ -17,14 +16,15 @@ def product_category_data_json(data):
     categorys = []
     for category in data:
         products = []
-        for product in data("products"):
-            products.append(product(**Product))
-        data("products") = products
-        categorys.append(category(**Category))
+        for product in category['products']:
+            products.append(Product(**product))
+        category['products'] = products
+        categorys.append(Category(**category))
     return categorys
 
 
-if __name__ == '__main__':
-    products = read_json('../data/products.json')
-    function = product_category_data_json(products)
+if __name__ == "__main__":
+    products_list = read_json("../data/products.json")
+    print(products_list)
+    function = product_category_data_json(products_list)
     print(function)
