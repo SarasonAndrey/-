6de3b2372bc8c4c293e5fd6a707ffd6c3b6ea63a
Andrey Sarason <sarason@rambler.ru>
2024-11-17 15:07:32 +0300

    modified:   src/category.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str  # Название
     description: str  # Описание
@@ -9,18 +12,33 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.number_of_categories += 1
-        Category.number_of_products += len(self.products)
+        Category.number_of_products += len(self.__products)
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, products_new: Product):
+        if isinstance(products_new, Product):
+            self.__products.append(products_new)
+            Category.count_products += 1
+        else:
+            raise TypeError
 
 
-# if __name__ == "__main__":
-#     category = Category("оборудование", "музыкальный", ["магнитола", "плеер"])
-#     category = Category("мебель", "для спальни", ["кровать", "диван"])
-#
-#     print(category.name)
-#     print(category.description)
-#     print(category.products)
-#     print(Category.number_of_categories)
-#     print(Category.number_of_products)
+
+if __name__ == "__main__":
+    category = Category("оборудование", "музыкальный", ["магнитола", "плеер"])
+    category = Category("мебель", "для спальни", ["кровать", "диван"])
+    category = Category("столы", "для кухни", ["складной стол", "стол тумба"])
+
+
+    print(category.name)
+    print(category.description)
+    print(category.products)
+    print(Category.number_of_categories)
+    print(Category.number_of_products)
