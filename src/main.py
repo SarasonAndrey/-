a629298b56e9c09a5ed91872eb36fd9d672c57db
Andrey Sarason <sarason@rambler.ru>
2024-12-03 16:20:57 +0300

    add branch 15_1

--- src/main.py
+++ src/main.py
@@ -29,14 +29,14 @@ if __name__ == "__main__":
         }
     )
     print(new_product.name)
-    # print(new_product.description)
-    # print(new_product.price)
-    # print(new_product.quantity)
-    #
-    # new_product.price = 800
-    # print(new_product.price)
-    #
-    # new_product.price = -100
-    # print(new_product.price)
-    # new_product.price = 0
-    # print(new_product.price)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
