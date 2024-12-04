4edd76749d5d1ae48a7a57dc7f988f8d35dcc65c
Andrey Sarason <sarason@rambler.ru>
2024-12-04 19:52:57 +0300

    added tests for new functionality

--- tests/test_category.py
+++ tests/test_category.py
@@ -13,8 +13,8 @@ def test_category2(category_2):
     assert category_2.description == "для спальни"
     assert category_2.products == ["кровать", "диван"]
 
-    assert category_2.number_of_categories == 1
-    assert category_2.number_of_products == 2
+    assert category_2.number_of_categories == 2
+    assert category_2.number_of_products == 4
 
 
 def test_category3(category_3):
@@ -29,12 +29,17 @@ def test_category_property(category_1):
     assert category_1.products == ["магнитола", "плеер"]
 
 
-def add_product(self, new_products: Product):
-    assert self.__products.append(new_products) == "Samsung Galaxy S23 Ultra"
-    assert Category.number_of_products == 1
+def test_add_product(category_add, product_1):
+    category_add.add_product(product_1)
+    assert Category.number_of_products == 4
 
 
-def products_list(self):
-    assert self == Product(
-        name="Патифон", description="старый", price="123.3", quantity="3"
-    )
+def test_products_list(product_1):
+    assert product_1.name == "Патифон"
+    assert product_1.description == "старый"
+    assert product_1.price == "123.3"
+    assert product_1.quantity == "3"
+
+
+def test_products_list(products_list):
+    assert Product(name="Патифон", description="старый", price="123.3", quantity="3")
