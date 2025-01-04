8934dba5e17194c35759d6b5f0b7ade17c1dc654
Andrey Sarason <sarason@rambler.ru>
2025-01-04 13:25:46 +0300

    added a method that calculates the average price tag

--- src/category.py
+++ src/category.py
@@ -50,3 +50,9 @@ class Category:
                 f"{product.quantity} шт.\n"
             )
         return products_list
+
+    def average_price(self):
+        try:
+            return round(sum([product.price for product in self.__products]) / len(self.__products), 2)
+        except ZeroDivisionError:
+            return 0
